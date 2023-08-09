27e9483b6c3d25f3f533ebf018fb2df54dbd24c5
AR247365 <lonewolf22081998@gmail.com>
2023-08-09 23:46:20 +0530

    Added React lazy

--- ecom/src/index.js
+++ ecom/src/index.js
@@ -5,18 +5,21 @@ import App from "./App"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { store } from "./store"
 import { Provider } from "react-redux"
-import Terms from "./components/Terms"
 import ShopContain from "./components/ShopContain"
+import { lazy, Suspense } from "react"
 
+const LazyTerms = lazy(() => import("./components/Terms"))
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/ShopContain" element={<ShopContain />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/terms" element={<LazyTerms />} />
+          <Route path="/ShopContain" element={<ShopContain />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   </Provider>
 )
